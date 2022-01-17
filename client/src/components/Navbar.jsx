19e19f4b05cd5be7ad071a92bc827b528a8eb073
SJuras <jurasstpko@gmailcom>
2022-01-17 23:29:02 +0100

    v1.1.4 react-icons installed. Working on Navbar component.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
 
+import logo from '../../images/logo.png';
 
 const Navbar = () => {
   return(
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+
+    </nav>
   );
 }
 
