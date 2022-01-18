a04314e3e2fd35ad4d2d99b4f55627835ea2d801
SJuras <jurasstpko@gmailcom>
2022-01-18 10:49:25 +0100

    v1.1.9 smart contract done and testing script. On to testing.

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 contract Transactions {
 
-  uint256 transactionCounter;
+  uint256 transactionCount;
 
   event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
 
@@ -19,16 +19,20 @@ contract Transactions {
 
   TransferStruct[] transactions;
 
-  function addToBlockchain() public {
-    
+  function addToBlockchain(address payable receiver, uint amount, string memory message, string keyword) public {
+    transactionCount += 1;
+    transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+
+    emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
   }
 
-  function addToBlockchain() public {
+  function getAllTransactions() public view returns (TransferStruct[] memory) {
+    return transactions;
 
   }
 
-  function addToBlockchain() public {
-
+  function getTransactionCount() public view returns (uint256) {
+    return transactionCount;
   }
 
 
---
+++ smart_contract/scripts/deploy.js
@@ -0,0 +1,24 @@
+
+
+const main = async () => {
+
+  const Transactions = await hre.ethers.getContractFactory("Transactions");
+  const transations = await Transactions.deploy("Hello, Hardhat!");
+
+  await transations.deployed();
+
+  console.log("Transactions deployed to:", transations.address);
+}
+
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch(error){
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+runMain();
