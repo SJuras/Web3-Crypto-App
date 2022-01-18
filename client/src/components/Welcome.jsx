27e77186bf0c77f33379e69d45f2ad698c9db061
SJuras <jurasstpko@gmailcom>
2022-01-18 10:09:56 +0100

    v1.1.7 Welcome component layout done.

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,12 +5,26 @@ import { BsInfoCircle } from 'react-icons/bs';
 
 import { Loader } from './';
 
-const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white hover:bg-[#2952e3] transition-all duration-300";
 
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+  />
+);
 
 const Welcome = () => {
 
   const connectWallet = () => {
+
+  }
+
+  const handleSubmit = () => {
 
   }
 
@@ -25,7 +39,7 @@ const Welcome = () => {
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
             Explore the crypto world. Buy and sell cryptocurrencies easily
           </p>
-          <button type="button" onClick={connectWallet} className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
+          <button type="button" onClick={connectWallet} className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd] duration-300">
             <p className="text-white text-base font-semibold">Connect Wallet</p>
           </button>
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
@@ -52,8 +66,44 @@ const Welcome = () => {
 
         <div className="flex flex-col flex-1 items-center justify-center w-full md:mt-0 mt-10">
           <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
-
+            <div className="flex justify-between flex-col w-full h-full">
+              <div className="flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={21} color="#fff" />
+                </div>
+                <BsInfoCircle fontSize={17} color="#fff" />
+              </div>
+              <div>
+                <p className="text-white font-light text-sm">
+                  0xvbcdhvcdvschvsd...vdhcvscs
+                </p>
+                <p className="text-white font-semibold text-lg mt-1">
+                  Ethereum
+                </p>
+              </div>
+            </div>
           </div>
+
+          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+            <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
+            <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}} />
+            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {}} />
+            <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}} />
+            <div className="h-[1px] w-full bg-gray-400 my-2" />
+
+            {false ? (
+              <Loader />
+            ) : (
+              <button
+                type="button"
+                onCLick={handleSubmit}
+                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer hover:bg-[#2952e3] duration-300"
+              >
+                Send Now
+              </button>
+            )}
+          </div>
+
         </div>
 
       </div>
